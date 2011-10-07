cfdfd3ac53a14e3c7a6fc91c9c3a1dfb5c0385fd
jeanluc <jeanluc@7331230f-294c-4faf-b969-2f526431e8c9>
2011-10-07 03:56:56 +0000

    Use vector instead of list in compact; a bit faster.  Also enable other relation.

--- +braidlab/@braid/private/compact_helper.cpp
+++ +braidlab/@braid/private/compact_helper.cpp
@@ -10,7 +10,6 @@
 // The sort-and-cancel algorithm was ripped from the braid::braidword class.
 
 #include <iostream>
-#include <list>
 #include <vector>
 #include <algorithm>
 #include "mex.h"
@@ -18,46 +17,36 @@
 extern void _main();
 
 
-// Delete two adjacent list entries specified by iterators.
-// it1 and it2 still adjacent after deletion, and point to the next
-// entries after the deleted ones.
 template<class T>
 inline
-void delete_two(T& b, typename T::iterator& it1, typename T::iterator& it2)
-{
-  it1 = b.erase(it1,++it2);
-  it2++ = it1;
-}
-
-
-template<class T>
-inline
-bool sort_and_cancel(T& bw)
+bool sort_and_cancel(T& b)
 {
   // Use the commutation relations to bring generators as far left as
   // possible.  Delete (sig)(-sig) sequences as we go.
   bool sw, anysw = false;
-  do {
-    sw = false;
-    for (typename T::iterator it1 = bw.begin(), it2 = ++(bw.begin());
-	 it2 != bw.end(); ++it1, ++it2)
-      {
-	if (*it1 == -(*it2))
-	  {
-	    // Two adjacent generators cancel: eliminate them.
-	    delete_two(bw,it1,it2);
-	    sw = anysw = true;
-
-	    if (it2 == bw.end()) break;
-	  }
-	else if (abs(abs(*it1) - abs(*it2)) > 1 && abs(*it1) > abs(*it2))
-	  {
-	    // Two adjacent generators commute: swap them.
-	    std::swap(*it1,*it2);
-	    sw = anysw = true;
-	  }
-      }
-  } while (sw);
+  do
+    {
+      sw = false;
+      for (int i = 0; i < b.size()-1; ++i)
+	{
+	  if (b[i] == -b[i+1] && b[i] != 0)
+	    {
+	      // Two adjacent generators cancel: eliminate them.
+	      b[i] = b[i+1] = 0;
+	      ++i;
+	      sw = anysw = true;
+	    }
+	  else if (abs(abs(b[i]) - abs(b[i+1])) > 1 && abs(b[i]) > abs(b[i+1]))
+	    {
+	      // Two adjacent generators commute: swap them.
+	      std::swap(b[i],b[i+1]);
+	      sw = anysw = true;
+	    }
+	}
+      // remove 0's from the vector.
+      b.erase(remove(b.begin(),b.end(),0),b.end());
+    }
+  while (sw);
 
   return anysw;
 }
@@ -68,7 +57,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   using std::cout;
   using std::endl;
   using std::max;
-  typedef std::list<int>::iterator listit;
+  typedef std::vector<int>::iterator vecit;
+  typedef std::vector<int>::const_iterator veccit;
 
   // Arguments checked and formatted in compact.m.
 
@@ -77,10 +67,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   const int N = max(mxGetM(wA),mxGetN(wA));
   int n = 1;
 
-  // Convert braid word to list.
-  /* Would it be faster to use a vector, and overwrite deleted
-     generators with zeros? */
-  std::list<int> bw;
+  // Convert braid word to vector.
+  std::vector<int> bw;
   for (int i = 0; i < N; ++i)
     {
       n = max(n,abs((int)w[i])+1);
@@ -89,22 +77,13 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
   sort_and_cancel(bw);
 
-  // Then eventually try to apply the other rules.
   //
-  // Suggestion: look for i -(i+1) -i and replace with
+  // Apply the orther relation, crudely.
   //
-  // i -(i+1) -i  =  i -(i+1)   -i   -(i+1) (i+1)
-  //              =  i   -i   -(i+1)   -i   (i+1)
-  //              =           -(i+1)   -i   (i+1)
-  //
-  // It doesn't change the length, but maybe then could lead to more
-  // cancellations, once we've done everything we can with the
-  // commutativity rule.
-  //
-  // Another option is to systematically list all shortening rules.
 
-#if 0
-  for (int j = 0; j < 1; ++j) // Doing this a few times often helps.
+#define BRAIDLAB_MORE_COMPACT
+#ifdef BRAIDLAB_MORE_COMPACT
+  for (int j = 0; j < 2; ++j) // Doing this a few times often helps.
     {
       bool any;
       do {
@@ -115,7 +94,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	    pat[0] = i;
 	    pat[1] = i+1;
 	    pat[2] = i;
-	    listit it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
+	    vecit it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
 	    if (it != bw.end())
 	      {
 		*(it++) = i+1;
@@ -164,12 +143,11 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     }
 #endif
 
-  // Now copy list bw to an mxArray of int32's.
+  // Now copy vector bw to an mxArray of int32's.
   plhs[0] = mxCreateNumericMatrix(1,bw.size(),mxINT32_CLASS,mxREAL);
   int *bwp = (int *)mxGetData(plhs[0]);
   int k = 0;
-  for (std::list<int>::const_iterator it = bw.begin();
-       it != bw.end(); ++it, ++k)
+  for (veccit it = bw.begin(); it != bw.end(); ++it, ++k)
     {
       bwp[k] = *it;
     }
