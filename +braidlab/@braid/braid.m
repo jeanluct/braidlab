6400a39114d83641609e91128884e71e3d3fc47b
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2011-10-15 16:39:17 +0000

    * Add 'abs' option for burau representation.  * Move entropy, burau, subbraid to external definitions.

--- +braidlab/@braid/braid.m
+++ +braidlab/@braid/braid.m
@@ -203,47 +203,6 @@ classdef braid
       wr = sum(sign(obj.word));
     end
 
-    function bs = subbraid(b,s)
-    %SUBBRAID   Extract a subset of strings from a braid.
-    %   BS = SUBBRAID(B,S) returns the subbraid BS, obtained by discarding
-    %   all strings in B but the ones specified in S.  S is a vector which
-    %   is a subset of 1:N, where N is the number of strings in the braid.
-    %
-    %   This is a method for the BRAID class.
-    %   See also BRAID.
-      bs = subbraid_helper(b,s);
-    end
-
-    function entr = entropy(b,tol,maxrep)
-    %ENTROPY   Topological entropy of a braid.
-    %   ENTR = ENTROPY(B) returns the 
-    %
-    %   This is a method for the BRAID class.
-    %   See also BRAID, BRAID.LENGTH, BRAID.INTAXIS.
-
-      lenfun = @intaxis; % length function: length or intaxis
-
-      if nargin < 2, tol = 1e-6; end
-      if nargin < 3, maxrep = 100; end
-
-      % Use a fundamental group generating set as the initial multiloop.
-      u = braidlab.loop(b.n);
-      entr0 = -1;
-      logL0 = log(lenfun(u));
-      for i = 1:maxrep
-	u = b*u;
-	logL = log(lenfun(u));
-	entr = logL-logL0;
-	% Check if we've congerved to desired tolerance.
-	if abs(entr-entr0) < tol, break; end
-	entr0 = entr; logL0 = logL;
-      end
-      if i == maxrep
-	warning('BRAIDLAD:braid:entropy', ...
-		'Failed to converge to desired tolerance.')
-      end
-    end
-
     function str = char(b)
     %CHAR   Convert braid to string.
     %
