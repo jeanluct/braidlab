e5e5017e3ec2d2b6053552d7ecaf0500f758dfc7
jeanluc <jeanluc@7331230f-294c-4faf-b969-2f526431e8c9>
2011-10-16 05:10:24 +0000

    * Modify tight.m to test for stretching braids using the monoid.  * monster.m returns the "monster" braid.

--- testing/tight.m
+++ testing/tight.m
@@ -1,16 +1,15 @@
+function tight
+
+import braidlab.*
+
 % Number of strands.
-n = 5;
+n = 4;
 % Word length to optimise.
 nw = 4;
-% Number of repetitions of the braid to check.
-nrep = 2;
 
-% For n = 5, nw = 4:
-% 
-%                       Burau entr  edges in TT
-%
-% 1  3 -2 -4		1.479388    5
-% 1 -3 -2  4		0.862555    6          not Burau sharp (1.087)
+%istight(monster)
+
+printbraids = false;
 
 N = n-1;
 
@@ -18,7 +17,7 @@ N = n-1;
 sig = -N*ones(1,nw);
 sig(1) = 1; sig(end) = -N-1;
 
-nwords = (2*N)^(nw-1)
+nwords = (2*N)^(nw-1);
 
 good = 0;
 bad = 0;
@@ -45,59 +44,95 @@ while 1
   % If nothing was changed, we're done.
   if ~incr, break; end
 
-  fprintf('%s  ',num2str(sig))
-  isbad = false;
-  for k = 1:n-1
-    fprintf(' l%d=',k)
-    b = zeros(1,n-2);
-    if k > 1, b(k-1) = -1; end
-    if k < n-1, b(k) = 1; end
-    u = loop([zeros(1,n-2) b]);
-    l0 = length(u)/2;
-    for i = 1:nrep
-      for j = 1:length(sig)
-	u = braid(sig(j),n)*u;
-	l = length(u)/2;
-	fprintf('%3d ',l-l0)
-	if l-l0 < 0
-	  fprintf(' ...bad')
-	  bad = bad + 1;
-	  isbad = true;
-	  break
-	end
-	l0 = l;
-      end
-      if isbad, break; end
-    end
-    if isbad, break; end
-  end
-  if ~isbad
+  % Check if all generators are present.
+  gens = unique(sort(abs(sig)));
+  if length(gens) ~= n-1, continue; end
+
+  if printbraids, fprintf('%s  ',num2str(sig)); end
+  if istight(sig,n)
     good = good + 1;
     goodones = [goodones; sig];
+  else
+    if printbraids, fprintf(' ...bad'); end
+    bad = bad + 1;
   end
-  fprintf('\n')
+  if printbraids, fprintf('\n'); end
 end
 
 fprintf('\ngood=%d  bad=%d  total=%d\n',...
 	good,bad,good+bad)
 fprintf('\n\nThe best ones (pA candidates):\n\n')
 
+warning('off','BRAIDLAD:braid:entropy:noconv');
+
 pAcand = [];
 for i = 1:size(goodones,1)
-  entr = log(max(abs(eig(burau(braid(goodones(i,:)))))));
-  gens = unique(sort(abs(goodones(i,:))));
-  if length(gens) == n-1
-    ispAcand = all(gens == 1:n-1);
-  else
-    ispAcand = false;
-  end
-  if entr > 1e-4 & ispAcand
-    pAcand = [pAcand; [goodones(i,:) entr]];
+  entr = entropy(braid(goodones(i,:)));
+  if entr > 1e-4
+    entrbnd = log(max(abs(eig(burau(braid(goodones(i,:)),'abs')))));
+    entrbur = log(max(abs(eig(burau(braid(goodones(i,:)))))));
+    pAcand = [pAcand; [goodones(i,:) entr entrbnd entrbur]];
   end
 end
 
 pAcand = sortrows(pAcand,nw+1);
+colstr = sprintf(['%-' num2str(length(num2str(pAcand(1,1:nw)))) 's'],'braid');
+fprintf('%s \tentr         entr(abs)    entr(Burau)\n',colstr)
+for i = [1 (find(diff(pAcand(:,nw+1)) > 1e-3)+1)']
+  fprintf('%s \t',num2str(pAcand(i,1:nw)))
+  fprintf('%7.4f      %7.4f      %7.4f\n', ...
+	  pAcand(i,nw+1),pAcand(i,nw+2),pAcand(i,nw+3))
+end
 
-for i = 1:size(pAcand,1)
-  disp([num2str(pAcand(i,1:nw)) '  ' num2str(pAcand(i,nw+1))])
+% =====================================================================
+function ee = istight(sig,n)
+
+import braidlab.*
+
+if nargin < 2
+  n = max(abs(sig))+1;
+end
+
+a = braid(sig,n);
+
+% Compute entropy (not too accurately, to save time).
+entr = a.entropy(1e-3,10);
+entrabs = log(max(abs(eig(burau(a,'abs')))));
+
+% Return true if the entropy is equal to the monoid bound.
+ee = abs(entr-entrabs) < 5e-3;
+
+% =====================================================================
+function ee = istightold(sig,n,nrep)
+
+% The old way: look at all loops between adjacent pairs.
+
+import braidlab.*
+
+% Number of repetitions of the braid to check.
+if nargin < 3
+  nrep = 2;
+end
+
+ee = true;
+for k = 1:n-1
+  fprintf(' l%d=',k)
+  b = zeros(1,n-2);
+  if k > 1, b(k-1) = -1; end
+  if k < n-1, b(k) = 1; end
+  u = loop([zeros(1,n-2) b]);
+  l0 = length(u)/2;
+  for i = 1:nrep
+    for j = 1:length(sig)
+      u = braid(sig(j),n)*u;
+      l = length(u)/2;
+      fprintf('%3d ',l-l0)
+      if l-l0 < 0
+	fprintf(' ...bad')
+	ee = false;
+	return
+      end
+      l0 = l;
+    end
+  end
 end
