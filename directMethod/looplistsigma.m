114b1d64450c91d0420db7d4ae52bdfe5470737f
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2012-01-19 00:34:40 +0000

    Modify looplistsigma.m to specify growth factor.

--- directMethod/looplistsigma.m
+++ directMethod/looplistsigma.m
@@ -1,37 +1,38 @@
-function up = looplistsigma(ii,n,imin,imax)
+function up = looplistsigma(br,n,imin,imax,gr)
 %LOOPLISTSIGMA   Apply generators to loops, keep only non-growing ones.
 %   L = LOOPLISTSIGMA(B,VMIN,VMAX) applies the braid B to a list of loops
 %   with indices bounded from below by the vector VMIN, and from above by
-%   the vector VMAX.  L is an array that contains only the non-growing
-%   loops, that is, those that do not grow exponentially under the action
-%   of B.
+%   the vector VMAX.  L is an array of loops that contains only the
+%   non-growing loops, that is, those that do not grow exponentially under
+%   the action of B.
 %
 %   L = LOOPLISTSIGMA(B,N,IMIN,IMAX) applies B to all loops for N particles,
 %   where the loop coordinates are all bounded by the scalars IMIN and IMAX.
+%
+%   L = LOOPLISTSIGMA(B,...,MAXGROWTH) discards loops whose length has grown
+%   by a factor greater than MAXGROWTH (default 3).
 
 % TODO: Make part of braid class.  Add threshold for "non-growing"?
 
 badbounds = 'Lower bounds must be less than or equal to upper.';
 
-% If the input ii is a braid object, convert it to a vector of generators.
-if isa(ii,'braidlab.braid')
-  sigma = double(ii.word);
-else
-  sigma = ii;
-end
+% If the input br is a braid object, convert it to a vector of generators.
+if isa(br,'braidlab.braid'), br = double(br.word); end
 
-if length(n) > 1
+if isvector(n)
+  % n is a vector: so it specifies VMIN; imin is VMAX and imax is MAXGROWTH.
   if any(n > imin), error(badbounds); end
-  up = looplistsigma_helper(sigma,n,imin).';
+  up = braidlab.loop(looplistsigma_helper(br,n,imin,imax).');
   return
 end
 
 if any(imin > imax), error(badbounds); end
+
 N = 2*n-4;
 
 % Apply generators to all loops, and return an array of non-growing loop
 % coordinates.
-dyn = looplistsigma_helper(sigma,imin*ones(1,N),imax*ones(1,N)).';
+dyn = looplistsigma_helper(br,imin*ones(1,N),imax*ones(1,N),gr).';
 
 % Convert to an array of loops.
 up = braidlab.loop(dyn);
