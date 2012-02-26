69e84ed34ac061f780e2c69f36b3a28a3360288d
jeanluc <jeanluc@7331230f-294c-4faf-b969-2f526431e8c9>
2012-02-26 01:02:09 +0000

    * Bugfix: compact_helper.cpp crashed when the final result was an empty braid.  * Remove the old (unused) algorithm from compact_helper.cpp.

--- +braidlab/@braid/private/compact_helper.cpp
+++ +braidlab/@braid/private/compact_helper.cpp
@@ -7,7 +7,6 @@
 // Use the group relations to shorten a braid word as much as
 // possible.
 
-// The sort-and-cancel algorithm was ripped from the braid::braidword class.
 // The commute-and-cancel algorithm is from Bangert et al. (2002).
 
 #include <iostream>
@@ -15,45 +14,9 @@
 #include <algorithm>
 #include "mex.h"
 
-#define BRAIDLAB_BANGERT_HEURISTIC // use algorithm of Bangert et al. (2002)
 #undef  BRAIDLAB_BANGERT_RESTORE
 
 extern void _main();
-
-
-template<class T>
-inline
-bool sort_and_cancel(T& b)
-{
-  // Use the commutation relations to bring generators as far left as
-  // possible.  Delete (sig)(-sig) sequences as we go.
-  bool sw, anysw = false;
-  do
-    {
-      sw = false;
-      for (mwIndex i = 0; i < b.size()-1; ++i)
-        {
-          if (b[i] == -b[i+1] && b[i] != 0)
-            {
-              // Two adjacent generators cancel: eliminate them.
-              b[i] = b[i+1] = 0;
-              ++i;
-              sw = anysw = true;
-            }
-          else if (abs(abs(b[i]) - abs(b[i+1])) > 1 && abs(b[i]) > abs(b[i+1]))
-            {
-              // Two adjacent generators commute: swap them.
-              std::swap(b[i],b[i+1]);
-              sw = anysw = true;
-            }
-        }
-      // remove 0's from the vector.
-      b.erase(remove(b.begin(),b.end(),0),b.end());
-    }
-  while (sw && b.size() > 1);
-
-  return anysw;
-}
 
 
 //
@@ -83,6 +46,8 @@ template<class T>
 inline
 bool commute_and_cancel(T& b, const int dir)
 {
+  if (b.empty()) return false;
+
   bool shorter = false;
   mwIndex pos0 = 0; // pos0 is the starting point of the "current" generator.
   do
@@ -134,6 +99,7 @@ bool commute_and_cancel(T& b, const int dir)
         } while (i+dir >= 0 && i+dir <= b.size()-1);
       // remove 0's from the vector.
       b.erase(remove(b.begin(),b.end(),0),b.end());
+      if (b.empty()) break;
       if (incrpos) ++pos0;
     } while (pos0 < b.size()-1);
 
@@ -164,78 +130,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
       bw.push_back(w[i]);
     }
 
-#ifdef BRAIDLAB_BANGERT_HEURISTIC
   // Try to commute_and_cancel from the left/right until nothing changes.
   while (commute_and_cancel(bw,1) || commute_and_cancel(bw,-1)) {}
-#else
-  sort_and_cancel(bw);
-
-  //
-  // Apply the orther relation, crudely.
-  //
-
-#define BRAIDLAB_MORE_COMPACT
-#ifdef BRAIDLAB_MORE_COMPACT
-  for (int j = 0; j < 2; ++j) // Doing this a few times often helps.
-    {
-      bool any;
-      do {
-        any = false;
-        for (int i = 1; i <= n-2; ++i)
-          {
-            std::vector<int> pat(3);
-            pat[0] = i;
-            pat[1] = i+1;
-            pat[2] = i;
-            vecit it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
-            if (it != bw.end())
-              {
-                *(it++) = i+1;
-                *(it++) = i;
-                *(it) =   i+1;
-                any = sort_and_cancel(bw);
-              }
-
-            pat[0] = -i;
-            pat[1] = -(i+1);
-            pat[2] = -i;
-            it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
-            if (it != bw.end())
-              {
-                *(it++) = -(i+1);
-                *(it++) = -i;
-                *(it) =   -(i+1);
-                any = sort_and_cancel(bw);
-              }
-
-            pat[0] = i+1;
-            pat[1] = i;
-            pat[2] = i+1;
-            it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
-            if (it != bw.end())
-              {
-                *(it++) = i;
-                *(it++) = i+1;
-                *(it) =   i;
-                any = sort_and_cancel(bw);
-              }
-
-            pat[0] = -(i+1);
-            pat[1] = -i;
-            pat[2] = -(i+1);
-            it = std::search(bw.begin(),bw.end(),pat.begin(),pat.end());
-            if (it != bw.end())
-              {
-                *(it++) = -i;
-                *(it++) = -(i+1);
-                *(it) =   -i;
-                any = sort_and_cancel(bw);
-              }
-          }
-      } while (any);
-    }
-#endif
-#endif
 
   // Now copy vector bw to an mxArray of int32's.
   plhs[0] = mxCreateNumericMatrix(1,bw.size(),mxINT32_CLASS,mxREAL);
