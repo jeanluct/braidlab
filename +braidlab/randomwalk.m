a588fa8c8d2b594c8ededce0e04e4019f068ef92
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2012-11-12 01:49:20 +0000

    Use MEX file to generate random walk.

--- +braidlab/randomwalk.m
+++ +braidlab/randomwalk.m
@@ -1,7 +1,7 @@
 function X = randomwalk(n,N,eps)
 %RANDOMWALK   Random walks on the unit square.
 %   XY = RANDOMWALK(N,K,EPS) generates N random walks of K steps of size EPS
-%   on the unit square.  XY has size [K 2 N].  The random walkers are
+%   on the unit square.  XY has size [K+1 2 N].  The random walkers are
 %   reflected when they hit the boundaries.
 %
 %   See also BRAID, BRAID.BRAID.
@@ -15,15 +15,22 @@ function X = randomwalk(n,N,eps)
 %
 % circular domain.
 
-% Particles are uniformly distributed in [0,1]^2.
-X0 = rand(1,2,n);
+if 1
+  % Call MEX file.
+  X = randomwalk_helper(n,N,eps);
+else
 
-% Displacement at random angle at each step.
-theta = 2*pi*rand(N,1,n);
-dX = eps*[cos(theta) sin(theta)];
+  % Particles are uniformly distributed in [0,1]^2.
+  X0 = rand(1,2,n);
 
-% Add up the displacements.
-X = cumsum([X0;dX]);
+  % Displacement at random angle at each step.
+  theta = 2*pi*rand(N,1,n);
+  dX = eps*[cos(theta) sin(theta)];
 
-% Reflect back into the interval [0,1] for each coordinate.
-X = mod((-1).^floor(X).*X,1);
+  % Add up the displacements.
+  X = cumsum([X0;dX]);
+
+  % Reflect back into the interval [0,1] for each coordinate.
+  X = mod((-1).^floor(X).*X,1);
+
+end
