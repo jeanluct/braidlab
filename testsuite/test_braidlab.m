02feaba4fd3f91e067f1b40ba158e19e2c66be56
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2013-08-03 10:27:07 -0500

    Rename conjTest to conjtestTest, to agree with function name.

--- testsuite/test_braidlab.m
+++ testsuite/test_braidlab.m
@@ -9,7 +9,7 @@ import matlab.unittest.*
 
 suite = TestSuite.fromFolder(pwd);
 %suite = TestSuite.fromClass(?braidTest);
-%suite = TestSuite.fromClass(?conjTest);
+%suite = TestSuite.fromClass(?conjtestTest);
 %suite = TestSuite.fromClass(?compactTest);
 runner = TestRunner.withTextOutput;
 res = runner.run(suite)
