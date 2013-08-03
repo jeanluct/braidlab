fdfa2b0b60e5f0b339dec763192fbbc696e408b0
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2013-08-03 18:08:14 -0500

    Fixed typos in complexity help.

--- +braidlab/@braid/complexity.m
+++ +braidlab/@braid/complexity.m
@@ -2,7 +2,7 @@ function c = complexity(b)
 %COMPLEXITY   Dynnikov-Wiest geometric complexity of a braid.
 %   C = COMPLEXITY(B) returns the Dynnikov-Wiest complexity of a braid:
 %
-%     C(B) = log_2|B.E| - log2|E|
+%     C(B) = log2|B.E| - log2|E|
 %
 %   where E is a canonical curve diagram, and |L| gives the number of
 %   intersections of the curve diagram L with the real axis.
@@ -13,7 +13,7 @@ function c = complexity(b)
 %   Journal of the European Mathematical Society 9 (2007), 801-840.
 %
 %   This is a method for the BRAID class.
-%   See also BRAID, BRAID.INTAXIS.
+%   See also BRAID, BRAID.LOOPCOORDS, LOOP.INTAXIS.
 
 E = braidlab.loop(b.n);
 c = log2(intaxis(b*E)-2) - log2(intaxis(E)-2);
