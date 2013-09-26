d12a0f0055e77b306b5f30b8d9fea5cec4664e2b
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2013-09-26 07:00:21 -0600

    Make compactTest more stringent, to detect bug where trivial braid was not returned.

--- testsuite/compactTest.m
+++ testsuite/compactTest.m
@@ -2,14 +2,20 @@ classdef compactTest < matlab.unittest.TestCase
       
   methods (Test)
     function test_compact_id(testCase)
+      % Verify that compacting the trivial braid returns the trivial braid.
       id = braidlab.braid([],5);
-      testCase.verifyEqual(id,id.compact);
+      testCase.verifyTrue(isempty(id.compact.word));
+
+      % Verify that compacting gives the trivial braid in simple cases.
+      id = braidlab.braid([1 -2 2 -1]);
+      testCase.verifyTrue(isempty(id.compact.word));
 
       id = braidlab.braid([1 2 1 -2 -1 -2],5);
-      testCase.verifyEqual(id,id.compact);
+      testCase.verifyTrue(isempty(id.compact.word));
     end
 
     function test_compact_random(testCase)
+      % Verify that compacting doesn't change the braid.
       rng('default')
       n = 10; % how many strings
       k = 30; % how many generators
