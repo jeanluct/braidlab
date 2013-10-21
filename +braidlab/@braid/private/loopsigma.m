8a2fbf0e7e9f913e376ab4004a7864545db85840
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2013-10-21 18:25:44 -0500

    Revert to max/min for pos/neg, since VPI now implements them (thanks John!).

--- +braidlab/@braid/private/loopsigma.m
+++ +braidlab/@braid/private/loopsigma.m
@@ -25,10 +25,7 @@ n = size(u,2)/2 + 2;
 a = u(:,1:n-2); b = u(:,(n-1):end);
 ap = a; bp = b;
 
-% The pure function defs don't work for vpi, since it doesn't overlad min/max.
-%pos = @(x)max(x,0); neg = @(x)min(x,0);
-function x = pos(x), x(find(x < 0)) = 0; end
-function x = neg(x), x(find(x > 0)) = 0; end
+pos = @(x)max(x,0); neg = @(x)min(x,0);
 
 for j = 1:length(ii)
   i = abs(ii(j));
@@ -66,5 +63,3 @@ for j = 1:length(ii)
   a = ap; b = bp;
 end
 up = [ap bp];
-
-end
