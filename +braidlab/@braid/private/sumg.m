bb11345974dc4361d587684d7b81bed4f5c663a8
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2013-10-21 17:33:49 -0500

    Checked that VPI does not return true for isinteger.

--- +braidlab/@braid/private/sumg.m
+++ +braidlab/@braid/private/sumg.m
@@ -9,10 +9,10 @@ function out = sumg( varargin )
 %   (maxint + 2) - 3 = maxint - 3
 %   maxint + (2 - 3) = maxint - 1
 
-if length(varargin ) == 1 % single argument returns input value
+if length(varargin) == 1 % single argument returns input value
   out = varargin{1};
 elseif length(varargin) > 2
-  % more than two arguments recurse binomially
+  % More than two arguments recurse binomially;
   % consider sorting to improve results,
   % e.g., ( 1 - 1 ) + maxint    will not overflow
   %       ( 1 + maxing) - 1     will overflow
@@ -21,9 +21,9 @@ else % two arguments are added
   a1 = varargin{1}; a2 = varargin{2};
   out = a1 + a2; % regular input
 
-  % we will perform the check only on integers
-  if isinteger(out)
-    % for integers, we have an upper and a lower boundary
+  % We will perform the check only on integers
+  if isinteger(out) % Note that VPI type fails this check, which is ok.
+    % For integers, we have an upper and a lower boundary.
     if ~( out > intmin(class(out)) && out < intmax(class(out)) )
       error('BRAIDLAB:braid:sumg:overflow',...
 	    'Summation of %d and %d has overflowed.', a1, a2)
