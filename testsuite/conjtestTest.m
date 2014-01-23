70b73e51e5a389b6513badee6ca7f87c717e4f17
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2014-01-23 07:02:11 -0600

    Minor variable name change.

--- testsuite/conjtestTest.m
+++ testsuite/conjtestTest.m
@@ -23,10 +23,10 @@
 classdef conjtestTest < matlab.unittest.TestCase
 
   properties
-    gen1
-    gen2
-    gen1c
-    gen2c
+    b1
+    b2
+    b1c
+    b2c
   end
 
   methods (TestMethodSetup)
@@ -36,32 +36,32 @@ classdef conjtestTest < matlab.unittest.TestCase
       % Close the braid.
       XY = braidlab.closure(XY);
 
-      testCase.gen1 = braidlab.braid(XY);
-      testCase.gen1c = testCase.gen1.compact;
+      testCase.b1 = braidlab.braid(XY);
+      testCase.b1c = testCase.b1.compact;
       testCase.verifyError(@() braidlab.braid(XY,pi/4), ...
 			   'BRAIDLAB:braid:color_braiding:coincidentproj');
-      testCase.gen2 = braidlab.braid(XY,-pi/4);
-      testCase.gen2c = testCase.gen2.compact;
+      testCase.b2 = braidlab.braid(XY,-pi/4);
+      testCase.b2c = testCase.b2.compact;
     end
   end
 
   methods (Test)
     function test_length(testCase)
-      testCase.verifyEqual(testCase.gen1.length,894);
-      testCase.verifyEqual(testCase.gen1c.length,14);
-      testCase.verifyEqual(testCase.gen2.length,400);
-      testCase.verifyEqual(testCase.gen2c.length,12);
+      testCase.verifyEqual(testCase.b1.length,894);
+      testCase.verifyEqual(testCase.b1c.length,14);
+      testCase.verifyEqual(testCase.b2.length,400);
+      testCase.verifyEqual(testCase.b2c.length,12);
     end
 
     function test_compact(testCase)
-      testCase.verifyTrue(testCase.gen1 == testCase.gen1c,...
-			  'Something went wrong when compacting gen1.');
-      testCase.verifyTrue(testCase.gen2 == testCase.gen2c,...
-			  'Something went wrong when compacting gen2.');
+      testCase.verifyTrue(testCase.b1 == testCase.b1c,...
+			  'Something went wrong when compacting b1.');
+      testCase.verifyTrue(testCase.b2 == testCase.b2c,...
+			  'Something went wrong when compacting b2.');
     end
 
     function test_conj(testCase)
-      [conj,C] = conjtest(testCase.gen1c,testCase.gen2c);
+      [conj,C] = conjtest(testCase.b1c,testCase.b2c);
       testCase.verifyTrue(conj,'Braids are not conjugate.');
       testCase.verifyEqual(C,braidlab.braid([-3 -2 -3 -1 -2 -3 1 2 1 2]));
     end
