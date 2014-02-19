eb7381aaedda6cb7e54fa5d37249719e454ccae2
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2014-02-18 18:56:25 -0600

    Experimental braid.recsigns method uses loopsigma to record signs of pos/neg ops.

--- +braidlab/@braid/private/loopsigma.m
+++ +braidlab/@braid/private/loopsigma.m
@@ -1,4 +1,4 @@
-function up = loopsigma(ii,u)
+function [varargout] = loopsigma(ii,u)
 %LOOPSIGMA   Act on a loop with a braid group generator sigma.
 %   UP = LOOPSIGMA(J,U) acts on the loop U (encoded in Dynnikov coordinates)
 %   with the braid generator sigma_J, and returns the new loop UP.  J can be
@@ -34,9 +34,9 @@ if isempty(ii)
 end
 
 % If MEX file is available, use that.
-if exist('loopsigma_helper') == 3
+if exist('loopsigma_helper') == 3 && nargout < 2
   if isa(u,'double') || isa(u,'single') || isa(u,'int32') || isa(u,'int64')
-    up = loopsigma_helper(ii,u);
+    varargout{1} = loopsigma_helper(ii,u);
     return
   end
 end
@@ -47,6 +47,13 @@ ap = a; bp = b;
 
 pos = @(x)max(x,0); neg = @(x)min(x,0);
 
+% If nargout > 1, record the state of pos/neg operators.
+recposneg = false;
+if nargout > 1, recposneg = true; end
+if recposneg
+  pn = zeros(size(u,1),length(ii),4);
+end
+
 for j = 1:length(ii)
   i = abs(ii(j));
   if ii(j) > 0
@@ -54,32 +61,65 @@ for j = 1:length(ii)
      case 1
       bp(:,1) = sumg( a(:,1) , pos(b(:,1)) );
       ap(:,1) = sumg( -b(:,1) , pos(bp(:,1)) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,1));
+        pn(:,j,2) = sign(bp(:,1));
+      end
      case n-1
       bp(:,n-2) = sumg( a(:,n-2) , neg(b(:,n-2)) );
       ap(:,n-2) = sumg( -b(:,n-2) , neg(bp(:,n-2)) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,n-2));
+        pn(:,j,2) = sign(bp(:,n-2));
+      end
      otherwise
       c = sumg( a(:,i-1), -a(:,i), -pos(b(:,i)), neg(b(:,i-1)) );
-      ap(:,i-1) = sumg( a(:,i-1), -pos(b(:,i-1)), -pos(sumg(pos(b(:,i)), c) ) );
+      ap(:,i-1) = sumg( a(:,i-1), -pos(b(:,i-1)), -pos(sumg(pos(b(:,i)), c)) );
       bp(:,i-1) = sumg( b(:,i), neg(c) );
-      ap(:,i) = sumg( a(:,i), -neg(b(:,i)), -neg( sumg(neg(b(:,i-1)), -c)) );
+      ap(:,i) = sumg( a(:,i), -neg(b(:,i)), -neg(sumg(neg(b(:,i-1)), -c)) );
       bp(:,i) = sumg( b(:,i-1), -neg(c) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,i));
+        pn(:,j,2) = sign(b(:,i-1));
+        pn(:,j,3) = sign(c);
+        pn(:,j,4) = sign( neg(b(:,i-1)) - c );
+      end
     end
   elseif ii(j) < 0
     switch(i)
      case 1
       bp(:,1) = sumg(-a(:,1), pos(b(:,1)) );
       ap(:,1) = sumg(b(:,1), -pos(bp(:,1)) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,1));
+        pn(:,j,2) = sign(bp(:,1));
+      end
      case n-1
       bp(:,n-2) = sumg(-a(:,n-2), neg(b(:,n-2)) );
       ap(:,n-2) = sumg(b(:,n-2), - neg(bp(:,n-2)) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,n-2));
+        pn(:,j,2) = sign(bp(:,n-2));
+      end
      otherwise
       d = sumg(a(:,i-1), -a(:,i), pos(b(:,i)), -neg(b(:,i-1)));
       ap(:,i-1) = sumg(a(:,i-1), pos(b(:,i-1)), pos(sumg(pos(b(:,i)),- d)) );
       bp(:,i-1) = sumg(b(:,i), -pos(d));
       ap(:,i) = sumg(a(:,i), neg(b(:,i)), neg(sumg(neg(b(:,i-1)), d)) );
       bp(:,i) = sumg(b(:,i-1), pos(d) );
+      if recposneg
+        pn(:,j,1) = sign(b(:,i));
+        pn(:,j,2) = sign(b(:,i-1));
+        pn(:,j,3) = sign(pos(b(:,i)) - d);
+        pn(:,j,4) = sign(d);
+      end
     end
   end
   a = ap; b = bp;
 end
-up = [ap bp];
+varargout{1} = [ap bp];
+
+if nargout > 1
+  pn = reshape(pn,[size(u,1) 4*length(ii)]);
+  varargout{2} = pn;
+end
---
+++ devel/pnstabilize.m
@@ -0,0 +1,29 @@
+% Check if pos/neg operators in loopsigma eventually stabilize.
+
+b = braid('psi',9);
+
+l = loop(b.n);
+
+nconv = 0;
+pnl = [];
+
+for i = 1:200
+  [l,pn] = recsigns(b,l);
+  pnl = [pnl ; pn];
+  if i > 1
+    if all(pnl(end,:) == pnl(end-1,:))
+      nconv = nconv + 1;
+      if nconv >= 5
+	fprintf('converged after %d iterations.\n',i)
+	break
+      end
+    else
+      nconv = 0;
+    end
+  end
+end
+
+imagesc(pnl.')
+colormap bone
+xlabel('iteration')
+ylabel('pos / neg')
