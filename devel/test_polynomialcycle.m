cfa430ee060dd30f0e3f47106d59c3851806c99c
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2014-02-20 10:16:27 -0600

    Examine the polynomials of a limit cycle.  Same roots except for unit circle?

---
+++ devel/test_polynomialcycle.m
@@ -0,0 +1,20 @@
+b = braid('psi',15);
+pn = pncycle(b);
+
+pl = polynomialcycle(b,pn);
+
+rl = [];
+
+for i = 1:size(pl,1)
+  r = roots(pl(i,:)).';
+  [~,i] = sort(abs(r),'descend');
+  rl = [rl ; r(i)];
+end
+
+rldiff = abs(rl  - repmat(rl(1,:),[size(rl,1) 1]));
+noteq = [];
+for i = 1:size(rldiff,1)
+  noteq = [noteq find(rldiff(i,:) > 1e-12)];
+end
+noteq= unique(noteq);
+fprintf('Differing roots have magnitude\n%s\n',num2str(abs(rl(1,noteq))))
