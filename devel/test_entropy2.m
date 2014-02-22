938436114a8134a81690f77638b5b4ed30b70871
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2014-02-22 14:01:59 -0600

    Seems like finding entropy from the exact matrix is way too slow, and quite inaccurate.

---
+++ devel/test_entropy2.m
@@ -0,0 +1,46 @@
+function test_entropy(n,tol)
+
+% Try new method of finding entropy, directly from the linearized action matrix.
+
+import braidlab.*
+
+if nargin < 1, n = 15; end  % Even n is terrible.  Try n=22
+if nargin < 2, tol = 1e-8; end
+
+b = braid('psi',n);
+
+r = psiroots(n);
+eex = log(max(abs(r)));
+
+fprintf('Old method:\n')
+
+tic
+tol = 1e-9;
+e1 = entropy(b,tol);
+toc
+fprintf('diff1=%.3e\n',abs(e1-eex))
+
+fprintf('New method:\n')
+
+tic
+e2 = entropy2(b,tol);
+toc
+
+fprintf('diff2=%.3e\n',abs(e2-eex))
+
+%================================================================
+function [varargout] = entropy2(b,tol,maxit,nconvreq)
+%ENTROPY   Topological entropy of a braid.
+
+if nargin < 2, tol = 1e-6; end
+
+% Get defaults for maxit and nconvreq from lacycle.
+if nargin < 3, maxit = []; end
+if nargin < 4, nconvreq = []; end
+
+M = lamat(b,maxit,nconvreq);
+
+opts.isreal = true;
+opts.tol = tol;
+
+varargout{1} = log(abs(eigs(M,1,'LM',opts)));
