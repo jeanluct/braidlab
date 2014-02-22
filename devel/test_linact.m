80125aa3c9737399fbc22b3efcc8db43f04e494d
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2014-02-22 10:11:52 -0600

    Method braid.linact added.  Gives effective linear action of braid on a given loop.

--- devel/test_linact.m
+++ devel/test_linact.m
@@ -6,9 +6,7 @@ for i = 1:Ntest
   b = braid('random',5,10);
   l = loop(randi(100,1,2*b.n-2)-50);
 
-  [l2,pn] = b*l;
-
-  M = update_rules_matrix(b,pn,size(l.coords,2));
+  [M,l2] = linact(b,l);
 
   if any((M*l.coords.').' - l2.coords)
     error('Not equal: %s',num2str((M*l.coords.').' - l2.coords))
