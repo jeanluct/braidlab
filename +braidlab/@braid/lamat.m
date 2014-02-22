6a53700f7a6f67e7e6f4e34ea03c8b7d5343f789
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2014-02-22 13:31:59 -0600

    Missing boilerplate in lamat and lapoly.

--- +braidlab/@braid/lamat.m
+++ +braidlab/@braid/lamat.m
@@ -12,6 +12,26 @@ function M = lamat(b,maxit,nconvreq)
 %   This is a method for the BRAID class.
 %   See also BRAID, LOOP, BRAID.LACYCLE, BRAID.LAMAT.
 
+% <LICENSE
+%   Copyright (c) 2013, 2014 Jean-Luc Thiffeault
+%
+%   This file is part of Braidlab.
+%
+%   Braidlab is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   Braidlab is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with Braidlab.  If not, see <http://www.gnu.org/licenses/>.
+% LICENSE>
+
+
 % Get defaults for maxit and nconvreq from lacycle.
 if nargin < 2, maxit = []; end
 if nargin < 3, nconvreq = []; end
