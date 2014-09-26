4989554ddb66891604320e5fde6d6103d54d9a38
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2014-09-26 10:46:20 -0500

    Add method braid.reducing, which calls find_reducing_curves.

---
+++ +braidlab/@braid/reducing.m
@@ -0,0 +1,31 @@
+function l = reducing(b)
+%REDUCING   Find reducing curves for a braid.
+%
+%   This is a method for the BRAID class.
+%   See also BRAID, LOOP.LOOP, BRAID.CYCLE, BRAID.CYCLEMAT.
+
+% <LICENSE
+%   Copyright (c) 2013, 2014 Jean-Luc Thiffeault
+%
+%   This file is part of Braidlab.
+%
+%   Braidlab is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   Braidlab is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with Braidlab.  If not, see <http://www.gnu.org/licenses/>.
+% LICENSE>
+
+% Test with the bad reducible braid:
+%   b = braid([-3  1 -4  2 -3 -1 -2  3 -2  4  3  4]);
+% Reducing curve is
+%   lred = loop([0 -1 0 0 0 0 0 1]);
+
+l = find_reducing_curves(b);
