54ec1c6b3ecdd43725c5d3b13871ce54e673b7c9
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2014-10-08 11:19:08 -0500

    Improve conversion back to coords.

--- devel/tests/loop_speedtest/loop_speedtest.m
+++ devel/tests/loop_speedtest/loop_speedtest.m
@@ -12,13 +12,13 @@ fprintf('Number of loops = %g\n',size(l,1));
 % Make a long braid as vector.
 b = int32((-1).^randi(2,1,k) .* randi(n-1,1,k));
 
-fprintf('\naction of braid on loops (vectors)... ')
+fprintf('\nAction of braid on loops (vectors)... ')
 tic
 l2 = loopsigma_helper(b,l);
 t1 = toc;
 fprintf(' (%f seconds)\n',t1)
 
-fprintf('\nconverting to array of loops... ')
+fprintf('\nConverting to array of loops... ')
 tic
 l3 = loop(l);
 t2 = toc;
@@ -26,19 +26,19 @@ fprintf(' (%f seconds)\n',t2)
 
 b = braid(b);
 
-fprintf('\naction of braid on array of loops (classes)... ')
+fprintf('\nAction of braid on array of loops (classes)... ')
 tic
 l4 = b*l3;
 t3 = toc;
 fprintf(' (%f seconds)\n',t3)
 
 % Check equality of two results.
-fprintf('\nverify equality... ')
+fprintf('\nVerify equality... ')
 tic
-for i = 1:size(l,1)
-  if any(l2(i,:) ~= l4(i).coords)
-    error('Coordinates don''t match.')
-  end
+% Convert back to array of coordinates.
+l4c = cell(size(l4)); [l4c{:}] = l4.coords; l4c = cell2mat(l4c);
+if any(l2(:) ~= l4c(:))
+  error('Coordinates don''t match.')
 end
 t4 = toc;
 fprintf(' (%f seconds)\n',t4)
