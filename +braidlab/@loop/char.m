9d402cc22e98bdba91d1fca3557892f6d9e39d5b
Jean-Luc Thiffeault <jeanluct@users.noreply.github.com>
2014-10-08 17:21:41 -0500

    Fix printing for loops with matrix coords, as well as intersec, intaxis, minlength.

---
+++ +braidlab/@loop/char.m
@@ -0,0 +1,55 @@
+function str = char(obj)
+%CHAR   Convert loop to string.
+%
+%   This is a method for the LOOP class.
+%   See also LOOP, LOOP.DISP.
+
+% <LICENSE
+%   Copyright (c) 2013, 2014 Jean-Luc Thiffeault
+%
+%   This file is part of Braidlab.
+%
+%   Braidlab is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   Braidlab is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with Braidlab.  If not, see <http://www.gnu.org/licenses/>.
+% LICENSE>
+
+if isscalar(obj)
+  if ~isa(obj(1).coords,'vpi')
+    objstr = num2str(obj.coords);
+  else
+    % The VPI num2str command is buggy on arrays.
+    objstr = num2str(obj.coords(1));
+    if obj.coords(1) < 0, objstr(1:3) = ''; else objstr(1:4) = ''; end
+    for i = 2:length(obj.coords)
+      oo = num2str(obj.coords(i)); oo(1:2) = '';
+      objstr = [objstr oo]; %#ok<AGROW>
+    end
+  end
+  nr = size(objstr,1); % number of rows
+  str = [repmat('(( ',nr,1) objstr repmat(' ))',nr,1)];
+else
+  str = [];
+  if size(obj,1) > size(obj,2)
+    for i = 1:size(obj,1)
+      str = [str ; char(obj(i,:))]; %#ok<AGROW>
+    end
+  else
+    str = char(obj(:,1));
+    for i = 2:size(obj,2)
+      objstr = char(obj(:,i));
+      nr = size(objstr,1); % number of rows
+      str = [str repmat('  ',nr,1) objstr]; %#ok<AGROW>
+    end
+  end
+end
+
