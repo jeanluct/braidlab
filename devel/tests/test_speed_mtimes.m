137ba83375608005bc67d725309e06ace271c7b4
Marko Budisic <mbudisic@gmail.com>
2015-07-07 12:48:58 -0500

    Test for comparison of (un)threaded multiplication speed

---
+++ devel/tests/test_speed_mtimes.m
@@ -0,0 +1,49 @@
+dbmex on
+global BRAIDLAB_debuglvl
+BRAIDLAB_debuglvl = 0
+
+braidlab.util.debugmsg('Initializing braid and loop')
+
+Lcoord = [1,1; 0,1; 1, 0; 0,-1; 3,2];
+Bcoord = [1,-2];
+
+Npoints = 10;
+Generators = ceil(logspace(1,4.5,Npoints));
+Loops = ceil(logspace(1,4,Npoints));
+
+
+timeS = nan([Npoints, Npoints]);
+timeM = nan([Npoints, Npoints]);
+
+for g = 1:Npoints
+  for l = 1:Npoints
+
+    NL = Loops(l);
+    NG = Generators(g);
+
+    L = braidlab.loop(repmat(Lcoord,ceil([NL/size(Lcoord,1),1])));
+    B = braidlab.braid(repmat(Bcoord, ceil([1, NG/numel(Bcoord)])));
+
+    braidlab.util.debugmsg('Multiplying')
+
+    clear getAvailableThreadNumber
+    global BRAIDLAB_threads
+    BRAIDLAB_threads = 1;
+    tic; LU = B*L; timeS(g,l) = toc;
+
+    clear getAvailableThreadNumber
+    clear('global','BRAIDLAB_threads')
+    tic; LT = B*L; timeM(g,l) = toc;
+
+    fprintf('Done: Nloops %d \t Ngen %d\n', NL, NG );
+
+  end
+end
+timeR = timeM ./ timeS;
+
+h=plot( Generators(:), timeR );
+labels = arrayfun( @(n)sprintf('Loops %d', n), Loops,'uniformoutput',false );
+[h.DisplayName] = deal(labels{:});
+xlabel('# generators');
+ylabel('Runtime');
+legend('Location','Best');
