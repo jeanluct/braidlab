496b5f2d2a5968017a57853fb44b5b4adb842765
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2016-06-20 12:37:14 -0400

    Check braid against discreteized trajectories.

--- devel/tests/test_colorbraiding_ODE/test_colorbraiding_ODE.m
+++ devel/tests/test_colorbraiding_ODE/test_colorbraiding_ODE.m
@@ -8,6 +8,7 @@ tmax = 20;
 
 f = @(t,X) rigid(t,X,omega);
 
+% Initial condition.
 XY0 = zeros(2,n);
 ang = 0;
 XY0(1,:) = cos(ang)*(1:n);
@@ -15,7 +16,26 @@ XY0(2,:) = sin(ang)*(1:n);
 rng('default')
 XY0 = XY0 + .001*rand(size(XY0));
 
+% Integrate some trajectories at discrete times.
+t = linspace(0,tmax,101);
+XY = zeros(length(t),2,n);
+for i = 1:n
+  [~,XY1] = ode45(f,t,XY0(:,i));
+  XY(:,:,i) = XY1;
+end
+
+% Construct braid from discretized trajectories.
+b0 = databraid(XY);
+
+% Construct braid directly from ODE.
 b = braid(f,[0 tmax],XY0);
+
+% Check if the braids match.
+if (b == b0)
+  fprintf('\nBraids match!\n')
+else
+  fprintf('\n*** Braids don''t match...\n')
+end
 
 %=======================================================================
 function dX = rigid(t,X,omega)
