d494f1a5a5eee36fb2ea4a0525ed665509530364
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2026-01-27 14:44:02 -0600

    Refactor conjtestTest.m: consistent naming and section headings.
    Reorganize tests into logical sections:
    - Self-conjugacy tests (2 tests).
    - Conjugate braids tests (3 tests).
    - Non-conjugate braids tests (3 tests).
    - Length tests (2 tests).
    - Compact equality tests (1 test).
    - Error tests (1 test).
    
    Expanded test coverage from 3 to 12 tests by splitting monolithic tests
    and adding new tests for self-conjugacy, non-conjugate braids with
    different n, and trivial vs non-trivial braids.
    
    Eliminated testCase properties (b1, b2, b1c, b2c) by loading test data
    directly in tests that need it.
    
    Use consistent test_<category>_<description> naming convention.

--- testsuite/testcases/conjtestTest.m
+++ testsuite/testcases/conjtestTest.m
@@ -1,6 +1,3 @@
-% Verify independence on projection line.
-% Braids should be conjugate.
-
 % <LICENSE
 %   Braidlab: a Matlab package for analyzing data using braids
 %
@@ -27,48 +24,140 @@
 
 classdef conjtestTest < matlab.unittest.TestCase
 
-  properties
-    b1
-    b2
-    b1c
-    b2c
-  end
+  methods (Test)
 
-  methods (TestMethodSetup)
-    function createBraids(testCase)
-      load('testdata','XY','ti')
-      XY = XY(1:length(ti),:,:); 
-      % Close the braid.
+    %% Self-conjugacy tests
+
+    function test_self_conjugate(testCase)
+      % A braid is conjugate to itself.
+      br = braidlab.braid([1 2 -3], 4);
+      isconj = conjtest(br, br);
+      testCase.verifyTrue(isconj);
+    end
+
+    function test_self_identity(testCase)
+      % Identity is conjugate to itself.
+      br = braidlab.braid([], 4);
+      isconj = conjtest(br, br);
+      testCase.verifyTrue(isconj);
+    end
+
+    %% Conjugate braids tests
+
+    function test_conjugate_simple(testCase)
+      % Test conjugate braids with simple conjugator.
+      br1 = braidlab.braid([1 2], 4);
+      c = braidlab.braid([1], 4);
+      br2 = c * br1 * c.inv;
+      isconj = conjtest(br1, br2);
+      testCase.verifyTrue(isconj);
+    end
+
+    function test_conjugate_returnsconjugator(testCase)
+      % Test that conjtest returns the conjugating braid.
+      br1 = braidlab.braid([1 2], 4);
+      br2 = braidlab.braid([1 -2 1 2 2 -1], 4);
+      [isconj, C] = conjtest(br1, br2);
+      testCase.verifyTrue(isconj);
+      testCase.verifyTrue(inv(C) * br1 * C == br2);
+    end
+
+    function test_conjugate_projection(testCase)
+      % Verify independence on projection line.  Braids should be conjugate.
+      load('testdata', 'XY', 'ti')
+      XY = XY(1:length(ti), :, :);
       XY = braidlab.closure(XY);
 
-      testCase.b1 = braidlab.braid(XY);
-      testCase.b1c = testCase.b1.compact;
-      testCase.verifyError(@() braidlab.braid(XY,pi/4), ...
+      br1 = braidlab.braid(XY);
+      br1c = br1.compact;
+      br2 = braidlab.braid(XY, -pi/4 + 1e-8);
+      br2c = br2.compact;
+
+      [conj, C] = conjtest(br1c, br2c);
+      testCase.verifyTrue(conj, 'Braids are not conjugate.');
+      testCase.verifyEqual(C, braidlab.braid([-3 -2 -3 -1 -2 -3 1 2 1 2]));
+    end
+
+    %% Non-conjugate braids tests
+
+    function test_nonconjugate_different(testCase)
+      % Test non-conjugate braids.
+      br1 = braidlab.braid([1 1], 3);
+      br2 = braidlab.braid([1 2], 3);
+      isconj = conjtest(br1, br2);
+      testCase.verifyFalse(isconj);
+    end
+
+    function test_nonconjugate_differentn(testCase)
+      % Braids with different n cannot be conjugate.
+      br1 = braidlab.braid([1 2], 4);
+      br2 = braidlab.braid([1 2], 5);
+      isconj = conjtest(br1, br2);
+      testCase.verifyFalse(isconj);
+    end
+
+    function test_nonconjugate_trivial_nontrivial(testCase)
+      % Trivial braid is not conjugate to non-trivial braid.
+      br1 = braidlab.braid([], 4);
+      br2 = braidlab.braid([1 2], 4);
+      isconj = conjtest(br1, br2);
+      testCase.verifyFalse(isconj);
+    end
+
+    %% Length tests (for projection-independent braids)
+
+    function test_length_projection1(testCase)
+      % Verify braid lengths for projection angle 0.
+      load('testdata', 'XY', 'ti')
+      XY = XY(1:length(ti), :, :);
+      XY = braidlab.closure(XY);
+
+      br = braidlab.braid(XY);
+      brc = br.compact;
+      testCase.verifyEqual(br.length, 894);
+      testCase.verifyEqual(brc.length, 14);
+    end
+
+    function test_length_projection2(testCase)
+      % Verify braid lengths for projection angle -pi/4.
+      load('testdata', 'XY', 'ti')
+      XY = XY(1:length(ti), :, :);
+      XY = braidlab.closure(XY);
+
+      br = braidlab.braid(XY, -pi/4 + 1e-8);
+      brc = br.compact;
+      testCase.verifyEqual(br.length, 400);
+      testCase.verifyEqual(brc.length, 12);
+    end
+
+    %% Compact equality tests
+
+    function test_compact_equality(testCase)
+      % Verify compact braids are equal to originals.
+      load('testdata', 'XY', 'ti')
+      XY = XY(1:length(ti), :, :);
+      XY = braidlab.closure(XY);
+
+      br1 = braidlab.braid(XY);
+      br1c = br1.compact;
+      testCase.verifyTrue(br1 == br1c, 'Something went wrong when compacting.');
+
+      br2 = braidlab.braid(XY, -pi/4 + 1e-8);
+      br2c = br2.compact;
+      testCase.verifyTrue(br2 == br2c, 'Something went wrong when compacting.');
+    end
+
+    %% Error tests
+
+    function test_error_coincidentprojection(testCase)
+      % Verify error on coincident projection.
+      load('testdata', 'XY', 'ti')
+      XY = XY(1:length(ti), :, :);
+      XY = braidlab.closure(XY);
+
+      testCase.verifyError(@() braidlab.braid(XY, pi/4), ...
                            'BRAIDLAB:braid:colorbraiding:coincidentprojection');
-      testCase.b2 = braidlab.braid(XY,-pi/4 + 1e-8);
-      testCase.b2c = testCase.b2.compact;
-    end
-  end
-
-  methods (Test)
-    function test_length(testCase)
-      testCase.verifyEqual(testCase.b1.length,894);
-      testCase.verifyEqual(testCase.b1c.length,14);
-      testCase.verifyEqual(testCase.b2.length,400);
-      testCase.verifyEqual(testCase.b2c.length,12);
     end
 
-    function test_compact(testCase)
-      testCase.verifyTrue(testCase.b1 == testCase.b1c,...
-                          'Something went wrong when compacting b1.');
-      testCase.verifyTrue(testCase.b2 == testCase.b2c,...
-                          'Something went wrong when compacting b2.');
-    end
-
-    function test_conj(testCase)
-      [conj,C] = conjtest(testCase.b1c,testCase.b2c);
-      testCase.verifyTrue(conj,'Braids are not conjugate.');
-      testCase.verifyEqual(C,braidlab.braid([-3 -2 -3 -1 -2 -3 1 2 1 2]));
-    end
   end
 end
