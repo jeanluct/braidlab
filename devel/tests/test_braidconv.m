0eec036227c495af0c47a66ab62f41640dafbe1a
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2026-03-22 13:59:27 -0500

    Delete the devel folder from release 3.3 branch

--- devel/tests/test_braidconv.m
+++
@@ -1,5 +0,0 @@
-b = braidconv([1 2 3])
-b.lr = -1     % change convention to right-to-left
-b.cw = -1     % change convention to counterclockwise
-b.word = [-4 -3 -2 -1] % now define word (read right-to-left)
-b.word(2) = 2 % change the second generator, read from right-to-left
